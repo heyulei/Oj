729e773db38f39cb8086a1676a7215ec0f01941f
heyulei <314525100@qq.com>
2019-12-03 16:46:29 +0800

    添加main函数方便调试

--- OJ2.java
+++ OJ2.java
@@ -60,3 +60,38 @@ public class Solution {
        
     }
 }
+/*public static ListNode creatnode(){
+        ListNode l1 = new ListNode(4);
+        ListNode l2 = new ListNode(1);
+        ListNode l3 = new ListNode(8);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+
+        return l1;
+
+    }
+    public static ListNode creatnode1(){
+        ListNode l1 = new ListNode(5);
+        ListNode l2 = new ListNode(0);
+        ListNode l3 = new ListNode(1);
+        ListNode l4 = new ListNode(8);
+        ListNode l5 = new ListNode(4);
+        ListNode l6 = new ListNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l6;
+        return l1;
+
+    }
+    public static void main(String[] args) {
+
+      ListNode l =  creatnode();
+      ListNode l1 = creatnode1();
+        getIntersectionNode(l,l1);
+   }*/
