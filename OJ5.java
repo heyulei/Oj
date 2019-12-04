27949aaa4f35ed11a870ff93a4439e1e0007f08f
heyulei <314525100@qq.com>
2019-12-04 12:41:56 +0800

    添加迭代用栈方法

--- OJ5.java
+++ OJ5.java
@@ -23,3 +23,20 @@ class Solution {
         return;
     }
 }
+/*public List<Integer> inorderTraversal(TreeNode root) {
+
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+        return list;
+    }
+    */
