b4cb9a0013612f9276d4cdcc6fb75abd37fa101b
heyulei <314525100@qq.com>
2019-12-06 00:46:33 +0800

    给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。

--- OJ6.java
+++ OJ6.java
@@ -10,7 +10,7 @@ public class TreeLinkNode {
     }
 }
 */
-public class Solution {
+/*public class Solution {
     public TreeLinkNode GetNext(TreeLinkNode pNode)
     {
         if(pNode.next==null){
@@ -43,6 +43,47 @@ public class Solution {
                 }
                 
                 }
+        }
+    }
+}*/
+
+
+
+/*
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
+*/
+public class Solution {
+    public TreeLinkNode GetNext(TreeLinkNode pNode)
+    {
+
+
+            if(pNode.right!=null){
+                    TreeLinkNode cur = pNode.right;
+                    while (cur.left!=null){
+                        cur = cur.left;
+                    }
+                    return cur;
+                }else {
+                     TreeLinkNode cur = pNode;
+                    while (cur.next!=null&&cur.next.right == cur){
+                        cur = cur.next;
+                    }
+                if(cur.next==null){
+                    return null;
+                }else {
+                    return cur.next;
+                }
+
+
         }
     }
 }
