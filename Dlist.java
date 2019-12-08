1c17e621c7cfcbcfbb292f23ab1be7c2063e4224
heyulei <314525100@qq.com>
2019-12-08 19:58:41 +0800

    增加 在所在下标后添加结点

--- Dlist.java
+++ Dlist.java
@@ -10,41 +10,72 @@ public class Dlist {
     }
 
     static class DList {
-        DListNode node = new DListNode(-1);//傀儡节点
-        public static void addLast(DListNode head, int val) {
-            if (head.next == null) {
-                addFirst( head,val);
-                return;
+        //DListNode node = new DListNode(-1);//傀儡节点
+        static class DList {
+            HomeWork12.DListNode node = new HomeWork12.DListNode(-1);//傀儡节点
+            public void addIndex(HomeWork12.DListNode head, int index, int data) {
+                HomeWork12.DListNode ret = null;
+                int size = size(head);
+                if (index == size){
+                    addLast(head,data);
+                    return;
+                }
+                if (size ==0 ){
+                    addFirst(head,data);
+                    return;
+                }
+                if (index>size||size<0){
+                    return;
+                }
+                HomeWork12.DListNode cur = head.next;
+                for (int i = 0;i < index; i++){
+                    cur = cur.next;
+                }
+                HomeWork12.DListNode newNode = new HomeWork12.DListNode(data);
+                newNode.next = cur.next;
+                newNode.pre = cur;
+                cur.next.pre = newNode;
+                cur.next = newNode;
             }
-            DListNode newNode = new DListNode(val);
-
-            newNode.next = head;
-            newNode.pev = head.pre.next;
-            head.pre.next = newNode;
-            head.pre = newNode;
-
-        }
-        public static void addFirst(DListNode head, int val) {
-            if (head.next == null) {
-                DListNode newNode = new DListNode(val);
-                head.next = newNode;
-                newNode.pre = head;
+            private int size (HomeWork12.DListNode head){
+                int size = 0;
+                HomeWork12.DListNode cur = head;
+                while (cur.next!=head){
+                    size++;
+                    cur = cur.next;
+                }
+                return size;
+            }
+            public static void addLast(HomeWork12.DListNode head, int val) {
+                if (head.next == null) {
+                    addFirst(head,val);
+                    return;
+                }
+                HomeWork12.DListNode newNode = new HomeWork12.DListNode(val);
                 newNode.next = head;
+                newNode.pre = head.next.pre;
+                head.pre.next = newNode;
                 head.pre = newNode;
-                return;
             }
-            DListNode newNode = new DListNode(val);
-            newNode.pre = head;
-            newNode.next = head.next;
-            head.next.pre = newNode;
-            head.next = newNode;
+            public static void addFirst(HomeWork12.DListNode head, int val) {
+                if (head.next == null) {
+                    HomeWork12.DListNode newNode = new HomeWork12.DListNode(val);
+                    head.next = newNode;
+                    newNode.pre = head;
+                    newNode.next = head;
+                    head.pre = newNode;
+                    return;
+                }
+                HomeWork12.DListNode newNode = new HomeWork12.DListNode(val);
+                newNode.pre = head;
+                newNode.next = head.next;
+                head.next.pre = newNode;
+                head.next = newNode;
+
+            }
+
 
         }
-        static void display(DListNode head){
-
-        }
-
-    }
     public static void main(String[] args) {
         DListNode head = new DListNode(-1);
         DList.addLast (head, 1);
